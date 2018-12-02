e0338bc33474a504f87313f29418333fe70e8f1f
sungy <sungy@github.com>
2018-12-02 10:13:45 +0800

    change vue/cli
    use vue/cli 3

--- src/main.js
+++ src/main.js
@@ -1,25 +1,8 @@
 import Vue from 'vue'
-
-import 'normalize.css/normalize.css'// A modern alternative to CSS resets
-
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-import '@/styles/index.scss' // global css
-
-import App from './App'
-import router from './router'
-
-Vue.use(ElementUI, {
-  size: 'medium'
-})
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App),
+}).$mount('#app')
--- src/router/_import_production.js
+++
@@ -1,3 +0,0 @@
-module.exports = file => () => import('@/views/' + file + '.vue')
-
-// export default file => () => import('@/views/' + file + '.vue')
